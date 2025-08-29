60420099bf1a3fbd43065b57207927917ef7db45
WyldCode / e-dimensionz, Inc <alex@e-dimensionz.com>
2025-08-29 14:15:57 +0300

    Update models.ts

--- src/models.ts
+++ src/models.ts
@@ -61,6 +61,7 @@ export interface IStyledProps {
 
 export interface IDocument {
   uri: string;
+  download_uri: string;
   fileType?: string;
   fileData?: string | ArrayBuffer;
   fileName?: string;
